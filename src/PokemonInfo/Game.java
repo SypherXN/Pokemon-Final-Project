5a8d73efd4932e8be277e99197bc032cf4929465
Matthew Tran <mattgtran2001@gmail.com>
2017-11-16 21:05:21 -0800

    asd

--- src/PokemonInfo/Game.java
+++ src/PokemonInfo/Game.java
@@ -7,17 +7,20 @@ public class Game {
 	 */
 
 	/************* Pokemon Types *************/
+	//String type
 	public static final Type
 	
 		Grass = new Type("Grass");
 	
 	/********** Base Pokemon Stats **********/
+	//String name, String firstType, String secondType, int hp, int attack, int defense, int speed, int number
 	public static final BasePokemon
 	
 		Bulbasaur = new BasePokemon("Bulbasaur", "Grass", null, 45, 49, 49, 45, 1);
 	
 	/************* Pokemon Moves *************/
 	public static final Moves
+	//String name, int attack, int pp
 		
 		Tackle = new Moves("Tackle", 15, 5);
 	
