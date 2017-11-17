989521d81e506a0459ae115f69f884c1936023ad
Matthew Tran <mattgtran2001@gmail.com>
2017-11-16 20:40:38 -0800

    Type Class

---
+++ src/PokemonInfo/Type.java
@@ -0,0 +1,14 @@
+package PokemonInfo;
+
+public class Type {
+
+	private final String firstType, secondType;
+	
+	public Type(String firstType, String secondType) {
+		
+		this.firstType = firstType;
+		this.secondType = secondType;
+		
+	}
+	
+}
