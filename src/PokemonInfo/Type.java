88c411220062396c42c6d3cf1eab9d3f72bf1d96
Matthew Tran <mattgtran2001@gmail.com>
2017-11-16 21:01:32 -0800

    Just now

--- src/PokemonInfo/Type.java
+++ src/PokemonInfo/Type.java
@@ -5,11 +5,23 @@ public class Type {
 	public final String type;
 	
 	/*Creates a reference for the types of the Pokemon
-	 * @param type - type of the Pokemon
+	 * @param type - name of the type
 	 */
 	public Type(String type) {
 		
 		this.type = type;
+		
+	}
+	
+	public boolean isEffective() {
+		
+		return false;
+		
+	}
+	
+	public boolean isResisted() {
+		
+		return false;
 		
 	}
 	
