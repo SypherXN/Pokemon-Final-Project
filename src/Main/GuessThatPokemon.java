71f0d86bf379b4a02a152e9ee18f588b1bc2a7d6
CS2 <CS2@BHS-CART-FF-12>
2017-12-12 11:53:03 -0800

    hi

--- src/Main/GuessThatPokemon.java
+++ src/Main/GuessThatPokemon.java
@@ -55,6 +55,7 @@ public class GuessThatPokemon {
 						
 						blank = blank.substring(0, i) + upperCharGuess + blank.substring(i);
 						System.out.println(blank);
+						//hi
 						
 					}
 			
